002d94b3ad2d4edbfa1a31af87116db1ae859550
re-nanashi <jfabro3124@gmail.com>
2021-08-16 17:21:44 +0800

    reorganize

--- ppp_c++/chapter_4/exercises/ex_04.cc
+++ ppp_c++/chapter_4/exercises/ex_04.cc
@@ -18,40 +18,64 @@ bool checkIfLegal(std::string user_input)
     return false;
 }
 
-int numberGuesser()
+int main()
 {
+    int input{0};
+    std::cout << "Please enter a number (1-100) for the computer to guess:  \n";
+    std::cin >> input;
+
+    if (input > 100 || input < 0)
+    {
+        std::cout << "Invalid input\n";
+        return 0;
+    }
+
+    //generate a range 1 - 100
     int arr[100];
     for (int i = 0; i < 100; ++i)
     {
         arr[i] = i + 1;
     }
-    int low = 0, high = 99, mid = 49;
-    int guess = arr[mid];
-}
-
-int main()
-{
-    int input{0}, counter;
-    std::cout << "Please enter a number to guess: (1-100)";
-    std::cin >> input;
-
-    if (input > 100 || input < 0)
-    {
-        std::cout << "Invalid input";
-        return 0;
-    }
 
     std::string q_ans{" "};
-    bool f_quit = false;
+    int counter{0};
 
-    while (!f_quit)
+    int low{0}, high{99};
+
+    while (counter < 7)
     {
+        int mid = (high + low) / 2;
+        int guess = arr[mid];
 
+        if (guess == input)
+        {
+            std::cout << "I get it now. The number you entered is " << guess << "\n";
+            return 0;
+        }
+
+        std::cout << "Is your number greater than " << guess << " ? Y/N \n";
         std::cin >> q_ans;
         if (!checkIfLegal(q_ans))
         {
-            std::cout << "input illegal\n";
+            std::cout << "Invalid inputl\n";
             continue;
         }
+
+        if (q_ans == "Y")
+        {
+            std::cout << "Ok let me rethink my guess. \n";
+            low = mid + 1;
+            ++counter;
+        }
+        else
+        {
+            std::cout << "Hmmmmmm. lower than that huh. \n";
+            high = mid - 1;
+            ++counter;
+        }
     }
+
+    std::cout << "You lied didn't you. \n";
+
+    return 0;
 }
