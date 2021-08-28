528f08a74815a5f334a2227fcc2172dad5118473
re-nanashi <jfabro3124@gmail.com>
2021-08-28 03:13:25 +0000

    init

---
+++ ppp_c++/chapter_5/ex_01.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "What have I been doing ?" << std::endl;
+    std::cout << "Definitely not worthwhile probably." << std::endl;
+
+    return 0;
+}
