e898ddb22d9aa462ce265a70c35f210daf38bf99
re-nanashi <jfabro3124@gmail.com>
2021-09-25 13:42:07 +0800

    lesson 24

---
+++ src/part_4/lesson_24/24.7_working_with_priority_queue_low.cc
@@ -0,0 +1,26 @@
+#include <functional>
+#include <iostream>
+#include <queue>
+
+int main()
+{
+    using namespace std;
+
+    // Define a priority_queue object with greater <int> as predicate
+    priority_queue<int, vector<int>, greater<int>> numsInPrioQ;
+
+    cout << "Inserting {10, 5, -1, 20} into the priority_queue" << endl;
+    numsInPrioQ.push(10);
+    numsInPrioQ.push(5);
+    numsInPrioQ.push(-1);
+    numsInPrioQ.push(20);
+
+    cout << "Deleting " << numsInPrioQ.size() << " elements" << endl;
+    while (!numsInPrioQ.empty())
+    {
+        cout << "Deleting topmost element " << numsInPrioQ.top() << endl;
+        numsInPrioQ.pop();
+    }
+
+    return 0;
+}
