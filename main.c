8c302dc04cf2100966320cde882cf77d8fd195d8
re-nanashi <jfabro3124@gmail.com>
2022-03-09 22:15:35 +0800

    today's lesson

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 
-float celcius_conv(int f) {
-    float var = (5/9.0) * (f - 32);
-    return var;
-}
-
 int main(void)
 {
-    for (int i = 0; i <= 300; i+=20) {
-        printf("%d\t%0.6f\n", i, celcius_conv(i));
-    }
-    return 0;
+	return 0;
 }
