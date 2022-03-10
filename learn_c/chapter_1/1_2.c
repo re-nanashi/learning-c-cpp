a764567a0d2a215da81920971b9f8fed2f4d917d
re-nanashi <jfabro3124@gmail.com>
2022-03-10 11:44:24 +0800

    exercises

---
+++ learn_c/chapter_1/1_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Test\t1");
+	return 0;
+}
--- learn_c/chapter_1/1_6.c
+++ learn_c/chapter_1/1_6.c
@@ -4,10 +4,8 @@ int main(void)
 {
 	int c;
 
-	printf("%d", EOF);
-	while ((c = getchar()) != EOF) {
+	while ((c = getchar()) != EOF)
 		putchar(c);
-	}
 
 	return 0;
 }
