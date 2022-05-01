595a63dfc204d9ccf6a4f90cb3084b0a9a3a008b
re-nanashi <jfabro3124@gmail.com>
2022-05-01 19:54:58 +0800

    Edit variable names

--- learn_c/chapter_1/1_13.c
+++ learn_c/chapter_1/1_13.c
@@ -5,60 +5,63 @@
 
 int main(void)
 {
-	int c, i, c_len, max_occur, state;
-	int len_of_words[10];
+	int ch, index, ch_length, max_occurence, state;
+	int words_len_array[10];
 
-	c_len = 0, max_occur = 0;
+	ch_length = 0, max_occurence = 0;
 	state = OUT;
-	for (i = 0; i < 10; ++i)
-		len_of_words[i] = 0;
 
-	while ((c = getchar()) != EOF) {
-		/* check if input is not whitespace */
-		if (c != ' ' && c != '\n' && c != '\t') {
+	for (index = 0; index < 10; ++index)
+		words_len_array[index] = 0;
+
+	while ((ch = getchar()) != EOF) {
+		/* check if input is not a whitespace char */
+		if (ch != ' ' && ch != '\n' && ch != '\t') {
 			state = IN;
-			++c_len;
+			++ch_length;
 		} else if (state) {
 			state = OUT;
 
-			if (c_len >= 1 && c_len <= 9)
-				++len_of_words[c_len - 1];
-			else if (c_len > 9)
-				++len_of_words[9];
+			if (ch_length >= 1 && ch_length <= 9)
+				++words_len_array[ch_length - 1];
+			else if (ch_length > 9)
+				++words_len_array[9];
 
-			c_len = 0;
+			ch_length = 0;
 		}
 	}
 
 	/* get the highest number of occurrence */
-	for (i = 0; i < 10; ++i) {
-		if (len_of_words[i] > max_occur)
-			max_occur = len_of_words[i];
+	for (index = 0; index < 10; ++index) {
+		if (words_len_array[index] > max_occurence)
+			max_occurence = words_len_array[index];
 	}
 
-	if (max_occur == 0) {
+	if (max_occurence == 0) {
 		printf("No input provided. Please try again.\n");
 		return 0;
 	}
 
 	/* print the histogram. */
-	for (int y = max_occur; y > 0; --y) {
+	for (int i = max_occurence; i > 0; --i) {
 		/* print y axis, number of occurrence */
-		printf("%d\t", y);
+		printf("%d\t", i);
 
-		for (int x = 0; x < 10; ++x) {
-			if (y == len_of_words[x]) {
+		for (int j = 0; j < 10; ++j) {
+			if (i == words_len_array[j]) {
 				printf("*\t");
-				--len_of_words[x];
+				--words_len_array[j];
 			} else
 				printf(" \t");
 		}
+
 		printf("\n");
 	}
 
-	/* print x axis, word lengths */
-	for (int x = 0; x < 10; ++x)
-		printf("%d\t", x);
+	/* print x axis */
+	for (int i = 0; i < 10; ++i) {
+		printf("%d\t", i);
+	}
 	printf(">10\n");
 
 	return 0;
